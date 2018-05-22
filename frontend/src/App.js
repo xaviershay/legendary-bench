922dd4256c367f30a2c4d6fe1d51d808ec4920e9
Xavier Shay <contact@xaviershay.com>
2018-05-22 11:24:10 -0700

    Initial frontend skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      "gameData": {"b": 2}
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/games/1')
+      .then(results => results.json())
+      .then(data => {
+        console.log(data)
+        this.setState("gameData": data)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          {JSON.stringify(this.state.gameData, null, 2)}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
