11720629628292f80d25cfb2bb81d7f177d9d73e
Xavier Shay <contact@xaviershay.com>
2018-05-22 12:57:42 -0700

    Fixup random again, this time with benchmarks I have more confidence in

--- api/benchmark/Main.hs
+++ api/benchmark/Main.hs
@@ -1,25 +1,56 @@
 module Main where
 
-import Random
+import           Control.DeepSeq       (deepseq)
+import           Criterion.Main
+import           Data.Foldable         (toList)
+import qualified Data.Sequence         as S
+import           System.Environment    (getArgs, withArgs)
+import           System.Random         (getStdGen, mkStdGen)
 import qualified System.Random.Shuffle as RS
-import System.Random (mkStdGen)
-import Criterion.Main
-import qualified Data.Sequence as S
-import Data.Foldable (toList)
 
+import Random
+
+-- Provide statistical benchmarks, but also "fixed amount" versions per method
+-- for ease of profiling.
 main = do
-  let rng = mkStdGen 0
-  let n = 1000000
+  args <- getArgs
+
+  case args of
+    ("--compare":xs) -> withArgs xs mainBench
+    ["--fixed-rs"] -> mainRS
+    ["--fixed-shuffle"] -> mainShuffle
+    ["--fixed-shuffleseq"] -> mainShuffleSeq
+    _ -> putStrLn "Valid args: --compare, --fixed-rs, --fixed-shuffle, --fixed-shuffleseq"
+
+n :: Int
+n = 100000
+
+f1 xs rng = RS.shuffle' xs (length xs) rng
+f2 xs rng = fst . shuffle rng $ xs
+f3 xs rng = fst . shuffleSeq rng $ xs
+
+mainRS         = runLoop f1 id
+mainShuffle    = runLoop f2 id
+mainShuffleSeq = runLoop f3 S.fromList
+
+runLoop testf dataf = do
+  let testData = dataf [0..n-1]
+
+  results <- sequence (replicate 10 (f testData))
+  putStrLn . show $ (map (\x -> x `deepseq` 1) results)
+
+  where
+    f testData = do
+          rng <- getStdGen
+          return $ testf testData rng
+
+mainBench = do
   let testData = S.fromList [0..n-1]
-  let testSeq = testData
-  let f1 xs = RS.shuffle' xs (length xs) rng
-  let f2 = fst . shuffle rng
-  let f3 = fst . shuffleSeq rng
 
   defaultMain [
     bgroup "shuffle"
-      [ bench "System.Random.Shuffle"     $ whnf f1 (toList testData)
-      , bench "our shuffle no conversion" $ whnf f3 testSeq
-      , bench "our shuffle"               $ whnf f2 (toList testData)
+      [ bench "System.Random.Shuffle" $ nfIO (f1 (toList testData) <$> getStdGen)
+      , bench "Random.shuffle"        $ nfIO (f2 (toList testData) <$> getStdGen)
+      , bench "Random.shuffleSeq"     $ nfIO (f3 testData          <$> getStdGen)
       ]
     ]
--- api/src/Random.hs
+++ api/src/Random.hs
@@ -2,41 +2,22 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 -- Fast, reversible perfect shuffling.
---
--- On my benchmarks this implementation was about 100x faster than
--- System.Random.Shuffle
 module Random
   ( shuffle
-  , unshuffle
   , shuffleSeq
+  , unshuffle
   ) where
 
 import           Data.Foldable (toList)
 import qualified Data.Sequence as S
 import           System.Random (RandomGen, mkStdGen, randomR)
 
--- A somewhat excessive typeclass to enable functions to be made generic across
--- different list like classes.
-class SeqLike m a where
-  toSeq :: m a -> S.Seq a
-  fromSeq :: S.Seq a -> m a
-  cons :: a -> m a -> m a
-
-instance SeqLike ([]) a where
-  toSeq = S.fromList
-  fromSeq = toList
-  cons = (:)
-
-instance SeqLike S.Seq a where
-  toSeq = id
-  fromSeq = id
-  cons = (S.<|)
-
 -- /O(nlog(n))/ Randomize the order of a list with a "perfect shuffle": each
 -- possible permuation is equally likely.
 -- See http://okmij.org/ftp/Haskell/perfect-shuffle.txt for a description of
 -- the algorithm. This implementation improves on it by using Data.Sequence
--- (which uses 2-3 finger trees) rather than a binary tree.
+-- (which uses 2-3 finger trees) rather than a binary tree. As a result, it's
+-- slightly faster.
 shuffle :: RandomGen gen => gen -> [a] -> ([a], gen)
 shuffle rng xs =
   let (sequence, rng') = rseq (length xs) rng in
@@ -52,39 +33,31 @@ shuffle rng xs =
 
       (accum `S.index` y) : f accum' ys
 
--- This implementation has radically slower performance than `shuffle`. I don't
--- know why.
+-- `shuffle` but for sequences. Is around 2-3x slower due to conversion
+-- overhead.
 shuffleSeq :: RandomGen gen => gen -> S.Seq a -> (S.Seq a, gen)
-shuffleSeq rng xs =
-  let xs' = xs in
-  let (sequence, rng') = rseq (S.length xs') rng in
+shuffleSeq gen xs =
+  -- Surprisingly, converting to/from a list here was slightly faster than
+  -- modifying the shuffle implementation to be "sequence native". Guess: less
+  -- allocation burden due to reduced number of total sequence operations.
+  let (xs', gen') = shuffle gen (toList xs) in
 
-  (f xs' sequence, rng')
-
-  where
-
-    f :: S.Seq a -> [Int] -> S.Seq a
-    f accum [] = mempty
-    f accum (y:ys) =
-      let accum' = S.deleteAt y accum in
-
-      (accum `S.index` y) S.<| f accum' ys
+  (S.fromList xs', gen')
 
 -- /O(nlog(n))/ Given a shuffled list and the RandomGen used to shuffle it,
 -- return the unshuffled list.
-unshuffle :: (SeqLike m a, RandomGen gen) => gen -> m a -> (m a, gen)
+unshuffle :: RandomGen gen => gen -> [a] -> ([a], gen)
 unshuffle rng xs =
-  let xs' = toSeq xs in
-  let (sequence, rng') = rseq (S.length xs') rng in
-  let unshuffled = fromSeq . f mempty . S.reverse $
-                     S.zip xs' (S.fromList sequence S.|> 0) in
+  let (sequence, rng') = rseq (length xs) rng in
+  let unshuffled = toList . f mempty . reverse $
+                     zip xs sequence in
 
   (unshuffled, rng')
 
   where
-    f :: S.Seq a -> S.Seq (a, Int) -> S.Seq a
-    f accum S.Empty = accum
-    f accum ((x, i) S.:<| xs) = f (S.insertAt i x accum) xs
+    f :: S.Seq a -> [(a, Int)] -> S.Seq a
+    f accum [] = accum
+    f accum ((x, i):xs) = f (S.insertAt i x accum) xs
 
 -- This implementation based on Data.Random.Shuffle, changed to return the
 -- generator. n must be greater than 1.
