0f0ca74e5c7aefae4b50f5af97ca202a42e1ca4f
Xavier Shay <contact@xaviershay.com>
2018-06-02 11:18:59 -0700

    Tidy ups to revert game to 'playable'

--- api/src/FakeData.hs
+++ api/src/FakeData.hs
@@ -64,7 +64,6 @@ mkPlayerDeck =
   sequence . fmap (mkCardInPlay Hidden) $
        S.replicate 8 moneyCard
     <> S.replicate 4 attackCard
-    <> S.replicate 40 (blackWidowCards !! 1)
 
 mkCardInPlay :: Visibility -> Card -> State Int CardInPlay
 mkCardInPlay visibility template = do
